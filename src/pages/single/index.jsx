66d8688198eb7b542240be8558f1ba1acd32ab1d
Jamkris <dltmdgus1412@gmail.com>
2024-05-24 14:23:18 +0800

    ADD :: 싱글페이지 퍼블리싱 완료

--- src/pages/single/index.jsx
+++ src/pages/single/index.jsx
@@ -1,13 +1,34 @@
 /*eslint-disable */
-import React from 'react';
+import React, { useState } from 'react';
+import DOMPurify from 'isomorphic-dompurify';
+
 import * as _ from './style';
+import Header from '../../components/header';
 
 const Single = () => {
-  return (
-    <div>
-      Single
-    </div>
-  );
+	const [title, setTitle] = useState('');
+	const [description, setDescription] = useState('');
+	const [date, setDate] = useState('');
+
+	const postId = window.location.pathname.split('/')[1];
+
+	return (
+		<_.Single_Container>
+			<Header />
+			<_.Single_Layout>
+				<_.Single_Title>{title}</_.Single_Title>
+				<_.Single_Date>{date}</_.Single_Date>
+
+				<_.Single_Body>
+					<div
+						dangerouslySetInnerHTML={{
+							__html: DOMPurify.sanitize(description ? description : ''),
+						}}
+					/>
+				</_.Single_Body>
+			</_.Single_Layout>
+		</_.Single_Container>
+	);
 };
 
 export default Single;
--- src/pages/single/style.js
+++ src/pages/single/style.js
@@ -1,2 +1,31 @@
 /*eslint-disable */
 import styled from 'styled-components';
+
+export const Single_Container = styled.div`width: 50vw;`;
+
+export const Single_Layout = styled.div`
+	width: 100vw;
+  padding: 20px 26vw;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: start;
+`;
+
+export const Single_Title = styled.div`
+	font-size: 40px;
+	font-family: 'Pretendard-Bold';
+`;
+
+export const Single_Date = styled.div`
+  font-family: 'Pretendard-Regular';
+  font-size: 14px;
+  color: #696969;
+
+`
+
+export const Single_Body = styled.div`
+	img {
+		max-width: 30vw;
+	}
+`;
