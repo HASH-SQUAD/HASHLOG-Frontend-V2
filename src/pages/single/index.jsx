82c68ebde10f6f1d7c725166e5ffe0859d6507ba
Jamkris <dltmdgus1412@gmail.com>
2024-06-04 17:22:58 +0900

    ADD :: 본인 게시글 표시
    자기 게시글이 아닐 경우 수정 및 삭제 버튼이 보이지 않음

--- src/pages/single/index.jsx
+++ src/pages/single/index.jsx
@@ -1,16 +1,17 @@
 /*eslint-disable */
 import React from 'react';
 import DOMPurify from 'isomorphic-dompurify';
+import Swal from 'sweetalert2';
 
 import * as _ from './style';
 import Header from '../../components/header';
 import { useQuery } from 'react-query';
 import { GetPostById } from '../../libs/api/Post';
-import Swal from 'sweetalert2';
+import { AuthState } from '../../libs/api/Auth';
 
 const Single = () => {
 	const postId = window.location.pathname.split('/')[1];
-	const { isLoading, isError, data, error } = useQuery(
+	const { isLoading, data: post } = useQuery(
 		['GetPostById', postId],
 		GetPostById,
 		{
@@ -22,6 +23,11 @@ const Single = () => {
 			},
 		}
 	);
+
+	const { data: auth, error } = useQuery('AuthState', AuthState, {
+		refetchOnWindowFocus: false,
+		retry: 0,
+	});
 
 	const Preparing = () => {
 		Swal.fire({
@@ -37,16 +43,20 @@ const Single = () => {
 		<_.Single_Container>
 			<Header />
 			<_.Single_Layout>
-				<_.Single_Title>{data?.data.title}</_.Single_Title>
+				<_.Single_Title>{post?.data.title}</_.Single_Title>
 				<_.Single_Info_Layout>
 					<_.Single_Info>
-						<_.Single_Nickname>{data?.data.User.nickname}</_.Single_Nickname>
-						<_.Single_Date>{data?.data.createdAt.substr(0, 10)}</_.Single_Date>
+						<_.Single_Nickname>{post?.data?.User?.nickname}</_.Single_Nickname>
+						<_.Single_Date>{post?.data.createdAt.substr(0, 10)}</_.Single_Date>
 					</_.Single_Info>
-					<_.Single_EditTools>
-						<button onClick={Preparing}>수정</button>
-						<button onClick={Preparing}>삭제</button>
-					</_.Single_EditTools>
+					{post?.data?.User?.nickname === auth?.data?.nickname ? (
+						<_.Single_EditTools>
+							<button onClick={Preparing}>수정</button>
+							<button onClick={Preparing}>삭제</button>
+						</_.Single_EditTools>
+					) : (
+						''
+					)}
 				</_.Single_Info_Layout>
 
 				<_.Single_Line></_.Single_Line>
@@ -54,7 +64,7 @@ const Single = () => {
 				<_.Single_Body>
 					<div
 						dangerouslySetInnerHTML={{
-							__html: DOMPurify.sanitize(data?.data ? data?.data.desc : ''),
+							__html: DOMPurify.sanitize(post?.data ? post?.data.desc : ''),
 						}}
 					/>
 				</_.Single_Body>
