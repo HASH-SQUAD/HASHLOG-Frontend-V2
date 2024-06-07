0a23c2ae66d8c1187c139adfee2778c33a699e19
Jamkris <dltmdgus1412@gmail.com>
2024-06-08 01:44:31 +0900

    ADD :: 헤더 추가

--- src/pages/setting/index.jsx
+++ src/pages/setting/index.jsx
@@ -2,9 +2,14 @@
 import React from 'react';
 
 import * as _ from './style.js';
+import Header from '../../components/header';
 
 const Setting = () => {
-	return <div></div>;
+	return (
+		<_.Setting_Container>
+			<Header />
+		</_.Setting_Container>
+	);
 };
 
 export default Setting;
