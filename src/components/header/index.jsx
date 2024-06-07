e333d1aa177fa623ae8f3eb2f51399c1beabd3de
Jamkris <dltmdgus1412@gmail.com>
2024-06-08 01:40:53 +0900

    CHORE :: 커밋 관리

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,4 @@
-///* eslint-disable */
+/* eslint-disable */
 import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useQuery } from 'react-query';
